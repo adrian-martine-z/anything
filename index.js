3503f4d05030bbe111b7f164feeac3d182119126
adrian-martine-z <jmtzadrian1@gmail.com>
2025-07-03 14:16:39 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,19 +5,19 @@ const port = 3000;
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/anything/index.html');
 });
 
 app.get('/index', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/anything/index.html');
 });
 
 app.get('/MacDeMarco', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/anything/index.html');
 });
 
 app.get('/about', (req, res) => {
-  res.sendFile(__dirname + '/about.html');;
+  res.sendFile(__dirname + '/anything/about.html');;
 });;
 app.get('/FreeTickets', (req, res) => {
   res.sendFile(__dirname + '/anything/freetickets.html');;
