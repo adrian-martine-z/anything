8de4ce041adc8bcfb17d8cda816ce4daa4e392bb
adrian-martine-z <jmtzadrian1@gmail.com>
2025-07-03 13:49:21 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express'); 
+const app = express(); 
+const port = 3000; 
+
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/homepage.html');
+});
+
+app.get('/Home', (req, res) => {
+  res.sendFile(__dirname + '/homepage.html');
+});
+
+app.get('/MacDeMarco', (req, res) => {
+  res.sendFile(__dirname + '/homepage.html');
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(__dirname + '/about.html');;
+});;
+app.get('/FreeTickets', (req, res) => {
+  res.sendFile(__dirname + '/freetickets.html');;
+});
+
+app.listen(port, () => {
+console.log(`Example app listening on port ${port}`)
+});
